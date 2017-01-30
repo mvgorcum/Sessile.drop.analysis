df3309e1bf0798b5337f293dd70424d5e658b55b
mvgorcum <mvgorcum@gmail.com>
2017-01-30 15:33:28 +0100

    Initial commit of the full script

---
+++ GUI_sessile_drop_analysis.py
@@ -0,0 +1,217 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan  5 13:20:32 2017
+
+@author: GorcumM
+"""
+
+import tkinter as tk
+import numpy as np
+from tkinter import ttk
+import matplotlib
+matplotlib.use("TkAgg")
+import matplotlib.pyplot as plt
+
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2TkAgg
+from matplotlib.figure import Figure
+from matplotlib import style
+
+LARGE_FONT= ("Verdana", 12)
+SMALL_FONT= ("Verdana", 8)
+style.use("ggplot")
+
+fig = Figure(figsize=(5,5),dpi=100)
+a = fig.add_subplot(111)
+
+def runscript(erfbool,k,II):
+    from contact_angle_analysis_function import analysis
+    global thetal, thetar, leftspeed, rightspeed, contactpointleft, contactpointright, dropvolume
+    thetal, thetar, leftspeed, rightspeed, contactpointleft, contactpointright, dropvolume = analysis(erfbool,k,II)
+    
+def plotstuff(typexplot,typeyplot,logxbool,logybool,pxscale,fps):
+    x=np.float()
+    y=np.float()
+    if typexplot==1:
+        x=thetal
+    elif typexplot==2:
+        x=thetar
+    elif typexplot==3:
+        x=dropvolume*pxscale**3
+    elif typexplot==3:
+        x=leftspeed*pxscale*fps
+    elif typexplot==4:
+        x=rightspeed*pxscale*fps
+    else:
+        print('no x variable set')
+    if typeyplot==1:
+        y=thetal
+    elif typeyplot==2:
+        y=thetar
+    elif typeyplot==3:
+        y=dropvolume
+    elif typeyplot==3:
+        y=leftspeed
+    elif typeyplot==4:
+        y=rightspeed
+    else:
+        print('no y variable set')
+
+    plt.plot(x,y)
+    if logxbool:
+        plt.xscale('log')
+    if logybool:
+        plt.yscale('log')
+    plt.show()
+
+class CAA(tk.Tk):
+    def __init__(self,*args,**kwargs):
+        tk.Tk.__init__(self,*args,**kwargs)
+        container=tk.Frame(self)
+        container.pack(side="top", fill="both", expand = True)
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        
+        self.frames = {}
+        
+        for F in (StartPage, PageTwo, PageThree):
+            frame = F(container,self)
+            self.frames[F]=frame
+            frame.grid(row=0, column=0,sticky= "nesw")
+        
+        self.show_frame(StartPage)
+        
+    def show_frame(self, cont):
+        frame = self.frames[cont]
+        frame.tkraise()
+        
+        
+def qf(stringtoprint):
+    print(stringtoprint)
+        
+#class StartPage(tk.Frame):
+#    def __init__(self, parent, controller):
+#        tk.Frame.__init__(self,parent)
+#        label = ttk.Label(self, text="Start Page", font=LARGE_FONT)
+#        label.pack(pady=10,padx=10)
+#        
+#        button1 = ttk.Button(self,text="visit page 1",command=lambda: controller.show_frame(PageOne))
+#        button1.pack()
+#        button2 = ttk.Button(self,text="visit graph page",command=lambda: controller.show_frame(PageTwo))
+#        button2.pack()
+
+        
+class StartPage(tk.Frame):
+
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        
+        label1 = ttk.Label(self, text="File analysis", font=LARGE_FONT)
+        label1.grid(row=0, column=1,pady=10,padx=10)
+        
+        labelk = ttk.Label(self, text="Amount of pixels from the baseline to fit:", font=SMALL_FONT)
+        labelk.grid(row=1, column=1)
+        kinput=ttk.Entry(self)
+        kinput.grid(row=2, column=1)
+        kinput.insert(0, 70)
+        
+        labelII = ttk.Label(self, text="Show output every 'x' frames:", font=SMALL_FONT)
+        labelII.grid(row=3, column=1)
+        IIinput=ttk.Entry(self)
+        IIinput.grid(row=4, column=1)
+        IIinput.insert(0, 5)
+        
+        labelpxscale = ttk.Label(self, text="Pixel size in mm per pixel:", font=SMALL_FONT)
+        labelpxscale.grid(row=5, column=1)
+        pxscaleinput=ttk.Entry(self)
+        pxscaleinput.grid(row=6, column=1)
+        pxscaleinput.insert(0, 1)
+        
+        labelfps = ttk.Label(self, text="Framerate of source", font=SMALL_FONT)
+        labelfps.grid(row=7, column=1)
+        fpsinput=ttk.Entry(self)
+        fpsinput.grid(row=8, column=1)
+        fpsinput.insert(0, 1)
+        
+        self.fasterbool = tk.BooleanVar()
+        self.fasterbool.set(False)
+        checkbox1 = ttk.Checkbutton(self,text="Faster execution (but less precise)",variable=self.fasterbool)
+        checkbox1.grid(row=9, column=1)
+        
+        button2 = ttk.Button(self,text="Go",command=lambda: runscript(self.fasterbool.get(),int(kinput.get()),int(IIinput.get())))
+        button2.grid(row=10, column=1)
+        
+        #button1 = ttk.Button(self,text="show plot",command=lambda: controller.show_frame(PageTwo))
+        #button1.grid(row=1, column=0)
+        
+        label2 = ttk.Label(self, text="Plot x variable", font=LARGE_FONT)
+        label2.grid(row=0,column=2,pady=10,padx=10)
+        self.typexplot=tk.IntVar()
+
+        ttk.Radiobutton(self, text="Theta left", variable=self.typexplot, value=1).grid(row=1, column=2)
+        ttk.Radiobutton(self, text="Theta right", variable=self.typexplot, value=2).grid(row=2, column=2)
+        ttk.Radiobutton(self, text="Volume", variable=self.typexplot, value=3).grid(row=3, column=2)
+        ttk.Radiobutton(self, text="Left speed", variable=self.typexplot, value=4).grid(row=4, column=2)
+        ttk.Radiobutton(self, text="Right speed", variable=self.typexplot, value=5).grid(row=5, column=2)
+        
+        self.logxbool = tk.BooleanVar()
+        self.logxbool.set(False)
+        
+        checkbox2 = ttk.Checkbutton(self,text="Logaritmic scale",variable=self.logxbool)
+        checkbox2.grid(row=6, column=2)
+        label3 = ttk.Label(self, text="Plot y variable", font=LARGE_FONT)
+        label3.grid(row=0,column=3,pady=10,padx=10)
+        self.typeyplot=tk.IntVar()
+
+        ttk.Radiobutton(self, text="Theta left", variable=self.typeyplot, value=1).grid(row=1, column=3)
+        ttk.Radiobutton(self, text="Theta right", variable=self.typeyplot, value=2).grid(row=2, column=3)
+        ttk.Radiobutton(self, text="Volume", variable=self.typeyplot, value=3).grid(row=3, column=3)
+        ttk.Radiobutton(self, text="Left speed", variable=self.typeyplot, value=4).grid(row=4, column=3)
+        ttk.Radiobutton(self, text="Right speed", variable=self.typeyplot, value=5).grid(row=5, column=3)
+        
+        self.logybool = tk.BooleanVar()
+        self.logybool.set(False)
+        checkbox2 = ttk.Checkbutton(self,text="Logaritmic scale",variable=self.logybool)
+        checkbox2.grid(row=6, column=3)
+        
+        button3 = ttk.Button(self,text="Show plot",command=lambda: plotstuff(self.typexplot.get(),self.typeyplot.get(),self.logxbool.get(),self.logybool.get(),np.float(pxscaleinput.get()),np.float(fpsinput.get())))
+        button3.grid(row=8,column=3)
+        #canvas._tkcanvas.pack()
+        
+class PageTwo(tk.Frame):
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = ttk.Label(self, text="Graph page", font=LARGE_FONT)
+        label.grid(row=0,column=0)
+        
+        button3 = ttk.Button(self,text="plot stuff",command=lambda: plotstuff())
+        button3.grid(row=3,column=0)
+        canvas = FigureCanvasTkAgg(fig,self)
+        canvas.show()
+        canvas.get_tk_widget().grid(row=0, column=1)
+        toolbar_frame = tk.Frame(self,parent)
+        toolbar_frame.grid(row=1,column=1)
+        toolbar = NavigationToolbar2TkAgg(canvas, toolbar_frame)
+        toolbar.update()
+        
+        button1 = ttk.Button(self,text="back home",command=lambda: controller.show_frame(StartPage))
+        button1.grid(row=0,column=0)
+
+class PageThree(tk.Frame):
+    def main(self):
+        self.thetal = np.random.rand(1,1)
+    def __init__(self,parent,controller):
+        tk.Frame.__init__(self,parent)
+        label = ttk.Label(self, text="Page one", font=LARGE_FONT)
+        label.pack(pady=10,padx=10)
+        
+        button1 = ttk.Button(self,text="back home",command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+        self.var1 = np.float()
+        button2 = ttk.Button(self,text="run script",command=self.main)
+        button2.pack()
+        button3 = ttk.Button(self,text="print thetal",command=lambda: print(thetal))
+        button3.pack()
+        
+app = CAA()
+
+app.mainloop()
---
+++ contact_angle_analysis_function.py
@@ -0,0 +1,188 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 16 12:47:56 2016
+
+@author: Mathijs van Gorcum
+requires skimage, imageio, tkinter, and shapely
+"""
+
+def analysis(faster_fit,k,II):
+    print(k)
+    import matplotlib.pyplot as plt
+    import numpy as np
+    import imageio
+    import os
+    if faster_fit:
+        from edge_detection import linear_subpixel_detection as edge
+    else:
+        from edge_detection import errorfunction_subpixel_detection as edge
+    
+    from skimage.viewer.canvastools import RectangleTool
+    from skimage.viewer.canvastools import LineTool
+    from skimage.viewer import ImageViewer
+    from skimage import io
+    from shapely.geometry import LineString
+    import tkinter as tk
+    from tkinter import filedialog
+    
+    PO=3 #polyfit order for edge fitting to measure contact angle
+    thresh=70 #replace with automatic treshold detection at some point
+    
+    #userinput for file, for now only movies, but image sequences will be added
+    root = tk.Tk()
+    root.withdraw()
+    filename = filedialog.askopenfilename()
+    
+    #filename='dummy file.avi' #to save time, test file
+    
+    filext=os.path.splitext(filename)[1]
+    
+    #check the file type
+    if filename.lower().endswith('.avi') or filename.lower().endswith('.mp4'):
+        filetype=0
+        global vid
+        vid = imageio.get_reader(filename,)
+        nframes = vid.get_meta_data()['nframes']
+    elif filename.lower().endswith(('.tiff', '.tif')):
+        tiffinfo=io.MultiImage(filename)
+        if len(tiffinfo)>1:
+            filetype=1
+            nframes=len(tiffinfo)
+        else:
+            import glob
+            filetype=2
+            filename=glob.glob(os.path.split(filename)[0]+os.sep+'*'+filext)
+            filename.sort()
+            nframes=len(filename)
+    elif filename.lower().endswith(('.png', '.jpg', '.jpeg')):
+        import glob
+        filetype=2
+        filename=glob.glob(os.path.split(filename)[0]+os.sep+'*'+filext)
+        filename.sort()
+        nframes=len(filename)
+    else:
+        print('unknown filetype')
+    
+        
+    #function to read a specific frame from the movie, stack, or image sequence
+    def getcurrframe(filename,framenr,filetype):
+        def movie():
+            image=vid.get_data(framenr)[:,:,0]
+            return image
+        def tifstack():
+            stack=io.imread(filename)
+            image=stack[framenr,:,:]
+            return image
+        def images():
+            image=io.imread(filename[framenr])
+            return image
+        filetypes={0 : movie,
+               1 : tifstack,
+               2 : images
+               }
+        image=filetypes[filetype]()
+        return image
+    
+    
+    image = getcurrframe(filename,0,filetype)
+    
+    #preallocate crop coordinates, maybe unescecarry?
+    coords=[0,0,0,0]
+    
+    #show the image and ask for a crop from the user, using skimage.viewer canvastools
+    viewer = ImageViewer(image)
+    rect_tool = RectangleTool(viewer, on_enter=viewer.closeEvent) #actually call the imageviewer
+    viewer.show() #don't forget to show it
+    coords=np.array(rect_tool.extents)
+    coords=np.array(np.around(coords),dtype=int)
+    
+    #crop the image
+    cropped=image[coords[2]:coords[3],coords[0]:coords[1]]
+    framesize=cropped.shape
+    
+    baseinput=np.array([0,0,0,0])
+    #userinput for the baseline, on which we'll find the contact angles, using skimage.viewer
+    viewer = ImageViewer(cropped)
+    line_tool=LineTool(viewer,on_enter=viewer.closeEvent)
+    viewer.show()
+    baseinput=line_tool.end_points
+    #extend the baseline to the edge of the frame (in case the drop grows)
+    rightbasepoint=np.argmax([baseinput[0,0],baseinput[1,0]])
+    baseslope=np.float(baseinput[rightbasepoint,1]-baseinput[1-rightbasepoint,1])/(baseinput[rightbasepoint,0]-baseinput[1-rightbasepoint,0])
+    base=np.array([[0,baseinput[0,1]-baseslope*baseinput[0,0]],[framesize[1],baseslope*framesize[1]+baseinput[0,1]-baseslope*baseinput[0,0]]])
+    
+    #preallocation of edges, angles and contact points
+    edgeleft=np.zeros(framesize[0])
+    edgeright=np.zeros(framesize[0])
+    thetal=np.zeros(nframes)
+    thetar=np.zeros(nframes)
+    contactpointright=np.zeros(nframes)
+    contactpointleft=np.zeros(nframes)
+    dropvolume=np.zeros(nframes)
+    plt.ion()
+    #loop over frames
+    for framenr in range (0,nframes):
+        image = getcurrframe(filename,framenr,filetype) #get current frame
+        cropped=np.array(image[round(coords[2]):round(coords[3]),round(coords[0]):round(coords[1])]) #crop frame
+        edgeleft, edgeright=edge(cropped,thresh) #find the edge with edge function in edge_detection.py
+        baseline=LineString(base) #using shapely we construct baseline
+        rightline=LineString(np.column_stack((edgeright,(range(0,framesize[0]))))) #and the lines of the edges
+        leftline=LineString(np.column_stack((edgeleft,(range(0,framesize[0])))))
+        leftcontact=baseline.intersection(leftline) #we find the intersectionpoint of the baseline with the edge
+        rightcontact=baseline.intersection(rightline)
+        #Detect small drops that are lower than 'k' pixels
+        #This may break if the drop grows outside the frame on one side. Maybe fix later?
+        fitpointsleft=edgeleft[range(np.int(np.floor(leftcontact.y)),np.int(np.floor(leftcontact.y)-k),-1)]
+        if any(fitpointsleft==0):
+            fitpointsleft=np.delete(fitpointsleft,range(np.argmax(fitpointsleft==0),k))
+        #polyfit the edges around the baseline, but flipped, because polyfitting a vertical line is bad
+        leftfit=np.polyfit(range(0,fitpointsleft.shape[0]),fitpointsleft,PO)
+        leftvec=np.array([1,leftfit[PO-1]]) #vector for angle calculation
+        fitpointsright=edgeright[range(np.int(np.floor(leftcontact.y)),np.int(np.floor(leftcontact.y)-k),-1)]
+        if any(fitpointsright==0):
+            fitpointsright=np.delete(fitpointsright,range(np.argmax(fitpointsright==0),k))
+        #polyfit the edges around the baseline, but flipped, because polyfitting a vertical line is bad
+        rightfit=np.polyfit(range(0,fitpointsright.shape[0]),fitpointsright,PO)
+        rightvec=np.array([1,rightfit[PO-1]]) #vector for angle calculation
+        basevec=np.array([-baseslope,1]) #base vector for angle calculation (allows for a sloped basline if the camera was tilted)
+        
+        #calculate the angles using the dot product.
+        thetal[framenr]=np.arccos(np.dot(basevec,leftvec)/(np.sqrt(np.dot(basevec,basevec))*np.sqrt(np.dot(leftvec,leftvec))))*180/np.pi
+        thetar[framenr]=180-np.arccos(np.dot(basevec,rightvec)/(np.sqrt(np.dot(basevec,basevec))*np.sqrt(np.dot(rightvec,rightvec))))*180/np.pi
+        if framenr % II ==0: #plot every II frames, to get a visual indication of how far along the script is
+                plt.clf()
+                plt.imshow(image[round(coords[2]):round(coords[3]),round(coords[0]):round(coords[1])],cmap='gray',interpolation="nearest")
+                plt.plot(edgeleft,range(0,framesize[0]))
+                plt.plot(edgeright,range(0,framesize[0]))
+                plt.plot([base[0,0],base[1,0]],[base[0,1],base[1,1]])
+                plt.title('frame %d of %d' % (framenr, nframes))
+                plt.pause(0.001)
+        #save the contact point (the point where the polyfit intersects the baseline)
+        contactpointright[framenr]=rightfit[PO]
+        contactpointleft[framenr]=leftfit[PO]
+        for height in range (0,min(np.int(np.floor(leftcontact.y)),np.int(np.floor(rightcontact.y)))):
+            dropvolume[framenr]=dropvolume[framenr]+np.pi*np.square((edgeright[height]-edgeleft[height])/2) #volume of each slice in pixels^3, without taking a possible slanted baseline into account
+        #using cylindrical slice we calculate the remaining volume
+        slantedbasediff=max(np.floor(leftcontact.y),np.floor(rightcontact.y))-min(np.floor(leftcontact.y),np.floor(rightcontact.y))
+        #we assume that the radius is constant over the range of the slanted baseline, for small angles this is probably accurate, but for larger angles this is probably wrong.
+        baseradius=(edgeright[np.int(min(np.floor(leftcontact.y),np.floor(rightcontact.y)))]-edgeleft[np.int(min(np.floor(leftcontact.y),np.floor(rightcontact.y)))])/2
+        dropvolume[framenr]=dropvolume[framenr]+.5*np.pi*np.square(baseradius)*slantedbasediff
+        
+    #%%
+   
+    fitsamplesize=3
+    leftspeed=np.zeros(nframes)
+    rightspeed=np.zeros(nframes)
+    for framenr in range(fitsamplesize,nframes-fitsamplesize-1):
+        rightposfit=np.polyfit(range(-fitsamplesize,fitsamplesize),contactpointright[range(framenr-fitsamplesize,framenr+fitsamplesize)],1)
+        leftposfit=np.polyfit(range(-fitsamplesize,fitsamplesize),contactpointleft[range(framenr-fitsamplesize,framenr+fitsamplesize)],1)
+        leftspeed[framenr]=leftposfit[0]
+        rightspeed[framenr]=rightposfit[0]
+    for fillinrest in range(0,fitsamplesize):
+        leftspeed[fillinrest]=leftspeed[fitsamplesize]
+        rightspeed[fillinrest]=rightspeed[fitsamplesize]
+    for fillinrest in range(nframes-fitsamplesize-1,nframes-1):
+        leftspeed[fillinrest]=leftspeed[nframes-fitsamplesize-1]
+        rightspeed[fillinrest]=rightspeed[nframes-fitsamplesize-1]
+    plt.close() #close the plot after we're done
+    return thetal, thetar, leftspeed, rightspeed, contactpointleft, contactpointright, dropvolume;  
